d296bf42d125003c019e793145aeb9b12b283293
Vlad Solovarov <vlad12342014@gmail.com>
2026-02-13 01:02:44 +0500

    before migration from sqlite to postgresql

--- app/database.py
+++ app/database.py
@@ -1,5 +1,19 @@
+from pydantic import SecretStr
 from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class ConfigBase(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='.env', env_file_encoding='utf-8', extra='ignore'
+    )
+
+class DatabaseConfig(ConfigBase):
+    model_config = SettingsConfigDict(env_prefix='DATABASE_')
+
+    URL: SecretStr
 
 
 DATABASE_URL = "sqlite:///ecommerce.db"
@@ -11,4 +25,16 @@ SessionLocal = sessionmaker(bind=engine)
 
 class Base(DeclarativeBase):
     pass
+#Async connection to PostgreSQL
+DATABASE_URL = DatabaseConfig.URL.get_secret_value()
 
+async_engine = create_async_engine(DATABASE_URL,
+                                   echo=True)
+
+async_session_maker = async_sessionmaker(async_engine,
+                                         expire_on_commit=False,
+                                         class_=AsyncSession)
+
+
+class Base(DeclarativeBase):
+    pass
